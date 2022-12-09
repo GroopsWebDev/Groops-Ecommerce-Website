e4b8a14ba3304a47e27a9f617b33124029006003
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-12-08 20:37:07 -0500

    fixed bugs

--- src/utils/get-stripe.js
+++ src/utils/get-stripe.js
@@ -1,11 +1,11 @@
 // ./utils/get-stripejs.ts
 import { Stripe, loadStripe } from '@stripe/stripe-js';
 
-let stripePromise: Promise<Stripe | null>;
+let stripePromise = null;
 
 export default function getStripe() {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
   }
   return stripePromise;
 };
