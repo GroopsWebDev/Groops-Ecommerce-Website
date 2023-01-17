03aa5d825109167fab408cdeb9290634b91e0176
Eddy Luo <eddy@MacBook-Pro.local>
2023-01-17 16:10:06 -0500

    add svg files

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -8,6 +8,9 @@ import WelcomeImage1 from "../../public/assets/welcome/welcome-img-1.svg";
 import WelcomeImage2 from "../../public/assets/welcome/welcome-img-2.svg";
 import WelcomeImage3 from "../../public/assets/welcome/welcome-img-3.svg";
 
+import OnSale from "../../public/assets/category/on-sale.svg"
+import PopularProduct from "../../public/assets/category/popular-product.svg"
+
 // import  from "../../public/assets/";
 
 const Product = () => {
@@ -15,6 +18,12 @@ const Product = () => {
     <>
       <div className="m-auto text-6xl">
         product page (member & guest)
+
+      </div>
+
+      <div className="flex flex-wrap">
+        <OnSale />
+        <PopularProduct />
       </div>
 
 
