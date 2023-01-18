40078033345d26d5c7e8ebd92983642aa2cd58e7
ratbuyer <m1662576219@gmail.com>
2023-01-18 01:03:57 -0500

    remove welcome image 1 since it's bigger

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -8,17 +8,21 @@ import WelcomeImage1 from "../../public/assets/welcome/welcome-img-1.svg";
 import WelcomeImage2 from "../../public/assets/welcome/welcome-img-2.svg";
 import WelcomeImage3 from "../../public/assets/welcome/welcome-img-3.svg";
 
+
+// removed for now due to size issue
+<Carousel.Item>
+  <div className="relative font-sans text-white">
+    <WelcomeImage1 className="w-full" />
+    <div className="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+      <ShopNowButton />
+    </div>
+  </div>
+</Carousel.Item>
+
+
 const Welcome = () => {
   return <>
     <Carousel>
-      <Carousel.Item>
-        <div className="relative font-sans text-white">
-          <WelcomeImage1 className="w-full" />
-          <div className="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 transform">
-            <ShopNowButton />
-          </div>
-        </div>
-      </Carousel.Item>
       <Carousel.Item>
         <div className="relative font-sans text-white">
           <WelcomeImage2 className="w-full" />
