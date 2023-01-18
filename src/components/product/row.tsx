ed56978dd8683aa6a966b960b8b40405d5acfea8
Yicong Luo <m1662576219@gmail.com>
2023-01-18 15:53:52 -0500

    product

--- src/components/product/row.tsx
+++ src/components/product/row.tsx
@@ -3,13 +3,16 @@ import React from "react"
 import Link from "next/link"
 
 import Detail from "./detail"
+
 import SeeAll from "../../../public/assets/shop/items/see-all.svg"
+import OnSale from "../../../public/assets/category/on-sale.svg"
 
 
 const Row = ()=> {
 
   return <>
-    <div className = "flex flex-wrap space-x-5 mb-10 mt-10 justify-center">
+  <OnSale className = "ml-10 mb-0"/>
+    <div className = "flex flex-wrap space-x-5 mb-10 justify-center">
       <Detail name = "product1" price = {1} heart = {false} />
       <Detail name = "product1" price = {1} heart = {false}/>
       <Detail name = "product1" price = {1} heart = {true} />
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -16,6 +16,7 @@ const Product = () => {
 
     <Welcome />
 
+    
     <Row />
     <Row />
 
