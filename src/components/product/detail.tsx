f7e6dd0b0ecf3882bc96962f6e0b04806afcaabd
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-01-18 20:55:09 -0500

    updated shop page (Henry)

--- src/components/product/detail.tsx
+++ src/components/product/detail.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState } from "react";
 
 import Container from "react-bootstrap/Container";
 
@@ -8,14 +9,19 @@ import HeartRed from "../../../public/assets/shop/items/heart-red.svg"
 import Add from "../../../public/assets/shop/items/add.svg"
 import Minus from "../../../public/assets/shop/items/minus.svg"
 
-type props = { name: string, price: number, heart: boolean }
+type props = { name: string, price: number}
 
-const Detail = ({ name, price, heart }: props) => {
+const Detail = ({ name, price }: props) => {
+  const [heart,setHeart] = useState(false)
+
   return <>
     <div className="relative text-2xl mb-10" >
       <Item className="w-100 h-100" />
+      <button onClick={()=>{setHeart(!heart)}}>
       {heart ? <HeartRed className="absolute bottom-10 right-5" /> :
-        <Heart className="absolute bottom-10 right-5" />}
+        <Heart className="absolute bottom-10 right-5"/>}
+      </button>
+
       <div className="flex gap-20 text-xs">
         <div className="flex flex-col space-y-0">
           <p className="ml-3  text-blue-400">{name}</p>
