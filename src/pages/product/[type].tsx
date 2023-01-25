a34a55ce5e2ffedde58aaeda87d8e2af5a56a015
ratbuyer <m1662576219@gmail.com>
2023-01-25 18:45:30 -0500

    update

--- src/pages/product/[type].tsx
+++ src/pages/product/[type].tsx
@@ -7,11 +7,61 @@ import Row from "../../components/shop/row"
 import HelpCenter from "../../components/help/help-center"
 
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 const Shop = () => {
 
   const Block = () => {
-    
+
+    const [filter, setFilter] = React.useState('');
+
+    const handleFilter = (event: SelectChangeEvent) => {
+      setFilter(event.target.value);
+    };
+
+    const [place, setPlace] = React.useState('');
+
+    const handlePlace = (event: SelectChangeEvent) => {
+      setPlace(event.target.value);
+    };
+
     return <>
+      <div className="flex flex-row justify-center space-x-10">
+
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="filter">Filter by</InputLabel>
+          <Select className = "w-40"
+            labelId="filter"
+            id="filter"
+            value={filter}
+            onChange={handleFilter}
+            label="Age"
+          >
+            <MenuItem value={"PHL"}>Price: High to Low</MenuItem>
+            <MenuItem value={"PLH"}>Price: Low to High</MenuItem>
+            <MenuItem value={"RHL"}>Rating: High to Low</MenuItem>
+            <MenuItem value={"RLH"}>Rating: Low to High</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel>Place of Product</InputLabel>
+          <Select className = "w-40"
+            value={place}
+            onChange={handlePlace}
+            label="Age"
+          >
+            <MenuItem value={"china"}>China</MenuItem>
+            <MenuItem value={"korea"}>Korea</MenuItem>
+            <MenuItem value={"japan"}>Japan</MenuItem>
+          </Select>
+        </FormControl>
+
+      </div>
+
       <Row type={type} />
       <Row type={type} />
       <Row type={type} />
@@ -27,10 +77,16 @@ const Shop = () => {
   return <>
 
     <Category />
+
     <Welcome />
-    <h1 className="text-purple-500 text-center mt-20 mb-20">{type}</h1>
+
+    <h1 className="text-purple-600 text-center mt-20 mb-20">{type}</h1>
 
     <Block />
+
+    <div className="flex justify-center mt-20">
+      <button className="bg-black w-40 hover:scale-110 text-white font-bold py-2 px-4 square">More</button>
+    </div>
 
     <HelpCenter />
   </>
