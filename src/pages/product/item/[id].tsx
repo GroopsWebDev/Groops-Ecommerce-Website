6e81b58bd6f50e887325593ad93f7ca3d27c7548
ratbuyer <m1662576219@gmail.com>
2023-02-10 23:00:22 -0500

    update

--- src/pages/product/item/[id].tsx
+++ src/pages/product/item/[id].tsx
@@ -1,18 +1,45 @@
 import React from "react";
+
+import Carousel from "react-bootstrap/Carousel";
+
 import { useRouter } from 'next/router';
 
-import Advertisement from "../../../../public/assets/shop/advertisement/advertisement.svg"
+import Advertisement from "../../../../public/assets/shop/advertisement/advertisement.svg";
+
+import DummyRecent from "../../../../public/assets/shop/items/item.svg";
+
+import HelpCenter from "../../../components/help/help-center";
+
+
+const ProductImages = () => {
+  return <>
+    <Carousel>
+
+      <Carousel.Item>
+      </Carousel.Item>
+
+      <Carousel.Item>
+      </Carousel.Item>
+
+    </Carousel>
+  </>
+}
+
+
+
 
 export default function Item() {
 
   const router = useRouter();
   const { id } = router.query;
 
-  
-
   return <>
-    {id}
+    <h1 className = "text-center">{id}</h1>
+
+    <ProductImages />
 
     <Advertisement className="w-full" />
+
+    <HelpCenter />
   </>
 }
