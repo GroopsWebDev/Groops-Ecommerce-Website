e184f9d1cc8a5973e9006bb34cbdde421a917336
ratbuyer <m1662576219@gmail.com>
2023-03-06 21:10:31 -0500

    category hover effects:

--- src/components/product/category.tsx
+++ src/components/product/category.tsx
@@ -12,21 +12,25 @@ import Kitchen from "../../../public/assets/category/kitchen.svg"
 import NewArrivals from "../../../public/assets/category/new-arrivals.svg"
 import Snack from "../../../public/assets/category/snack.svg"
 import Personal from "../../../public/assets/category/personal-care.svg"
+import { string } from "zod";
 
 const Catergory = () => {
 
+  style: string
+  let style = "hover:scale-110"
+
   return <>
     <div className="flex flex-wrap w-full gap-10 mt-10 mb-10">
-      <Link href="/product/On Sale"> <OnSale /> </Link>
-      <Link href="/product/Popular Products"> <PopularProduct /> </Link>
-      <Link href="/product/Drinks"> <Drinks /> </Link>
-      <Link href="/product/Frozen Food"> <FrozenFood /> </Link>
-      <Link href="/product/Groceries"> <Groceries /> </Link>
-      <Link href="/product/Instant Food"> <InstantFood /> </Link>
-      <Link href="/product/Kitchen"> <Kitchen /> </Link>
-      <Link href="/product/New Arrivals"> <NewArrivals /> </Link>
-      <Link href="/product/Snacks"> <Snack /> </Link>
-      <Link href="/product/Personal Care"> <Personal /> </Link>
+      <Link href="/product/On Sale" className={style}> <OnSale /> </Link>
+      <Link href="/product/Popular Products" className={style}> <PopularProduct /> </Link>
+      <Link href="/product/Drinks" className={style}> <Drinks /> </Link>
+      <Link href="/product/Frozen Food" className={style}> <FrozenFood /> </Link>
+      <Link href="/product/Groceries" className={style}> <Groceries /> </Link>
+      <Link href="/product/Instant Food" className={style}> <InstantFood /> </Link>
+      <Link href="/product/Kitchen" className={style}> <Kitchen /> </Link>
+      <Link href="/product/New Arrivals" className={style}> <NewArrivals /> </Link>
+      <Link href="/product/Snacks" className={style}> <Snack /> </Link>
+      <Link href="/product/Personal Care" className={style}> <Personal /> </Link>
     </div>
   </>
 
