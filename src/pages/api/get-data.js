58d25507de193877c868a69066bf495f666e0f61
Taiyi Xu <txu259@uwo.ca>
2023-03-11 17:23:08 -0500

    Removign all existing file

--- src/pages/api/get-data.js
+++
@@ -1,6 +0,0 @@
-import { prisma } from "../../../server/db/client";
-
-export default async function GetData(req, res) {
-  const product = await prisma.product.findMany();
-  return res.json({ status: 200, product });
-}
