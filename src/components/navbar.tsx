5a30dfd50e1c46fbea33f176c69c42612e49d243
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-03-25 23:37:38 -0400

    added user center routing -Henry

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -139,7 +139,7 @@ const Header = () => {
                       <Popover.Body className="bg-gradient-to-br from-purple-500 to-pink-400 p-1">
                         <div className="h-full w-full bg-white ">
                           <Link
-                            href="/member/profile"
+                            href="/member/userCenter"
                             className="ml-2 text-lg text-black no-underline hover:text-orange-500 hover:underline"
                           >
                             Account Setting
