93dea65d50e3052f3420a9822aea599e75912ecc
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-05-13 13:05:21 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { type NextPage } from "next";
+import Link from "next/link";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import { UserButton, useAuth } from "@clerk/nextjs";
+
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const db_users = api.example.getAllUser.useQuery();
+
+  return (
+    <>
+      <SignedIn>
+        <div>This content is visible only to signed in users.</div>
+        {isLoaded && userId && (
+          <>
+            <h2>Logged in as {userId}</h2>
+            <UserButton afterSignOutUrl="http://localhost:3000" />
+          </>
+        )}
+      </SignedIn>
+      <SignedOut>
+        {/* 
+              Route matches, but no user is signed in. 
+              Redirect to the sign in page.
+            */}
+
+        <RedirectToSignIn />
+      </SignedOut>
+
+      {db_users.data?.map((db_user) => (
+        <div key={db_user.id}>{db_user.email}</div>
+      ))}
+    </>
+  );
+};
+
+export default Home;
