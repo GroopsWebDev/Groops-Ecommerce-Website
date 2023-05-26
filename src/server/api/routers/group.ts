6a4422981bd9bccc711a67ba01a7e20b9ce65a98
Kua Chen <kua.chen@mail.mcgill.ca>
2023-05-26 14:33:30 +0800

    npm install

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -15,6 +15,41 @@ export const groupRouter = createTRPCRouter({
       return ctx.prisma.group.findUnique({
         where: { groupId: input.groupId }
       })
-    })
+    }),
+
+    createOrChangeAddress: publicProcedure
+    .input(z.object({
+      id: z.number(),
+      userId: z.string(),
+      primary: z.boolean(),
+      line1: z.string(),
+      line2: z.string(),
+      city: z.string(),
+      state: z.string(),
+      country: z.string(),
+    }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.address.upsert({
+        where: { id: input.id },
+        update: {...input},
+        create: {
+          userId: input.userId,
+          primary: input.primary,
+          line1: input.line1,
+          line2: input.line2,
+          city: input.city,
+          state: input.state,
+          country: input.country,
+        }
+      })
+    }),
+
+  deleteAddress: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.love_list.delete({
+        where: { ...input }
+      });
+    }),
 
 });
