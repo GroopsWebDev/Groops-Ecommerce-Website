7693b6acc86a68195c62f6c6b57e92c7158a63df
ratbuyer <m1662576219@gmail.com>
2023-06-01 23:29:20 -0400

    product image

--- src/pages/demo/product.tsx
+++ src/pages/demo/product.tsx
@@ -16,13 +16,24 @@ export default function Test() {
       <Link href="/" className="bg-black p-1 text-white">
         Back to home page
       </Link>
-      
+
       <h1 className="mb-10 mt-10 text-center">My user Id: {userId}</h1>
 
       <div className="flex flex-col place-items-center">
-        {data?.map((product) => (
-          <div key={product.skuid}>product name: {product.englishProductName}</div>
-        ))}
+        {data?.map((product, index) => {
+          const url = "https://api.gr-oops.com/" + product.image_url;
+          if (index < 5) {
+            return <>
+              <div key={index} className="mt-10">
+                <p>product name: {product.englishProductName}</p>
+                <img
+                  src={url}
+                  className="w-20"
+                />
+              </div>
+            </>
+          }
+        })}
       </div>
     </>
   );
