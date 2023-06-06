cb99e4068e3e94a3c921f5dcdf39e66a732285a7
ratbuyer <m1662576219@gmail.com>
2023-06-06 15:57:05 -0400

    fix svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>original home moved to /demo</h1>
+      <h1 className="text-center text-5xl mt-20">original content moved to /demo</h1>
     </div>
   )
 }
