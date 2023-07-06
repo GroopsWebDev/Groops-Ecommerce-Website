c41ea9e9bfec261c4ecd2fe8b4cc9fa406d0f5a7
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-07-06 22:26:28 +0800

    updated account center

---
+++ src/components/account/info2.tsx
@@ -0,0 +1,153 @@
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
+
+const PersonalInfo: React.FC = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+  return (
+    <>
+      <div className="rounded-lg bg-white p-10 text-gray-800 shadow-lg">
+        <div className="col-span-full">
+          <label
+            htmlFor="photo"
+            className="block font-medium leading-6 text-gray-900"
+          >
+            Photo
+          </label>
+          <div className="ml-0 mt-2 flex items-center gap-x-3">
+            {user!.profileImageUrl ? (
+              <Image
+                className="h-12 w-12 rounded-full"
+                src={user!.profileImageUrl}
+                alt=""
+                width={100}
+                height={100}
+              />
+            ) : (
+              <UserCircleIcon
+                className="h-20 w-20 p-0 text-gray-300"
+                aria-hidden="true"
+              />
+            )}
+
+            <button
+              type="button"
+              className="rounded-md bg-white px-2.5 py-1.5 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              Change
+            </button>
+          </div>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                User Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block font-medium leading-6 text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block font-medium leading-6 text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            {/* Email address */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            {/* Phone Number */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Phone Number
+              </label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 flex items-center">
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country"
+                    className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm"
+                  >
+                    <option>US</option>
+                    <option>CA</option>
+                  </select>
+                </div>
+                <input
+                  type="text"
+                  name="phone-number"
+                  id="phone-number"
+                  className="block w-full rounded-md border-0 py-1.5 pl-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6"
+                  placeholder="+1 (555) 987-6543"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PersonalInfo;
--- src/components/navbar/sidebar.tsx
+++ src/components/navbar/sidebar.tsx
@@ -7,13 +7,15 @@ import Logout from "@public/assets/icons/logout.svg";
 import Slide from "@mui/material/Slide";
 
 import Link from "next/link";
+import Image from "next/image";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 import React, { useEffect, useRef } from "react";
 import {
-  ClerkProvider,
   SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
+  useUser,
 } from "@clerk/nextjs";
 
 const Sidebar = ({
@@ -25,11 +27,13 @@ const Sidebar = ({
 }) => {
   const CrossButton = () => (
     <button className="absolute right-5 top-5" onClick={() => setShow(false)}>
-      <Cross />
+      <Cross className="text-white" />
     </button>
   );
 
   const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+  const { isLoaded, isSignedIn, user } = useUser();
 
   useEffect(() => {
     let handler = (e: any) => {
@@ -53,58 +57,66 @@ const Sidebar = ({
           <div className="h-full bg-white text-gray-700 shadow-2xl">
             <CrossButton />
 
-            <div className="flex flex-col items-center bg-rose-600">
-              {/* <img
-                src="/assets/dummy/product.png"
-                className="mt-10 w-1/2 rounded-full"
-              /> */}
+            {user ? (
+              <div>
+                <div className="flex flex-col items-center bg-rose-600">
+                  <Image
+                    src={user!.profileImageUrl}
+                    alt="clerk user.imageUrl"
+                    width={100}
+                    height={100}
+                    className="mt-8 h-32 w-32 rounded-full"
+                  />
+                  <h2 className="mt-5 text-2xl font-bold text-white">
+                    {/* First Name . L */}
+                    {user!.firstName} . {user!.lastName?.charAt(0)}
+                  </h2>
+                  <div className="mb-5 mt-5 w-[95%] rounded-xl bg-white p-5">
+                    <p className="text-2xl font-bold text-rose-600">$ 135</p>
+                    <p className="text-gray-400">Savings last 12 months</p>
+                  </div>
+                </div>
+                <div className="mt-10 flex h-full flex-col gap-y-6 pl-5 text-gray-700">
+                  <Link className="flex items-center" href="/account/1">
+                    <Account />
+                    <p className="ml-2">Your Account</p>
+                  </Link>
 
-              <SignedIn>
-                <UserButton
-                  appearance={{
-                    elements: {
-                      userButtonBox:"mt-10",
-                      userButtonAvatarBox: "w-36 h-36",
-                    },
-                  }}
-                />
-              </SignedIn>
-              <h2 className="mt-5 text-2xl font-bold text-white">
-                First Name . L
-              </h2>
+                  <Link className="flex items-center" href="/order">
+                    <Orders />
+                    <p className="ml-2">Your Orders</p>
+                  </Link>
 
-              <div className="mb-5 mt-5 w-[95%] rounded-xl bg-white p-5">
-                <p className="text-2xl font-bold text-rose-600">$ 135</p>
-                <p className="text-gray-400">Savings last 12 months</p>
+                  <Link className="flex items-center" href="/group">
+                    <Groups />
+                    <p className="ml-2">Your Groups</p>
+                  </Link>
+
+                  <Link className="flex items-center" href="/lovelist">
+                    <Heart className="w-5" />
+                    <p className="ml-2">Your LoveList</p>
+                  </Link>
+
+                  <Link className="flex items-center" href="/">
+                    <Logout />
+                    <p className="ml-2">Logout</p>
+                  </Link>
+                </div>
               </div>
-            </div>
-
-            <div className="mt-10 flex h-full flex-col gap-y-6 pl-5 text-gray-700">
-              <Link className="flex items-center" href="/account/1">
-                <Account />
-                <p className="ml-2">Your Account</p>
-              </Link>
-
-              <Link className="flex items-center" href="/order">
-                <Orders />
-                <p className="ml-2">Your Orders</p>
-              </Link>
-
-              <Link className="flex items-center" href="/group">
-                <Groups />
-                <p className="ml-2">Your Groups</p>
-              </Link>
-
-              <Link className="flex items-center" href="/lovelist">
-                <Heart className="w-5" />
-                <p className="ml-2">Your LoveList</p>
-              </Link>
-
-              <Link className="flex items-center" href="/">
-                <Logout />
-                <p className="ml-2">Logout</p>
-              </Link>
-            </div>
+            ) : (
+              <div>
+                <UserCircleIcon
+                  className="mt-8 h-32 w-32 text-gray-300"
+                  aria-hidden="true"
+                />
+                <button
+                  type="button"
+                  className="rounded-md bg-white  hover:bg-gray-50"
+                >
+                  Log In
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </Slide>
