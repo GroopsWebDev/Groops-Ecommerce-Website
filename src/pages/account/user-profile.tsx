e2c62af5505f40f26cc5d080aa6debf29ca68fbc
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-07-09 16:12:52 +0800

    updated UserProfilePage

--- src/pages/account/user-profile.tsx
+++ src/pages/account/user-profile.tsx
@@ -2,7 +2,13 @@ import { UserProfile } from "@clerk/nextjs";
 
 const UserProfilePage = () => (
   <div className="flex justify-center items-center">
-  <UserProfile />
+  <UserProfile 
+   appearance={{
+    elements: {
+      profileSectionPrimaryButton__username: 'hover:bg-rose-100',
+      nternal_rscu79: 'text-rose-100', //not working
+    }
+  }} />
   </div>
 
   );
