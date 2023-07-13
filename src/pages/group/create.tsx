6b7478c0fabdcbab3201ae11b3c7b408e810ed66
ratbuyer <m1662576219@gmail.com>
2023-07-12 22:59:12 -0400

    progressbar

--- src/pages/group/create.tsx
+++ src/pages/group/create.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import ProgressBar from "@components/group/progressBar";
 
 const CreateGroupPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <>
-      <h2>page {currentPage}</h2>
-    </>
+    <div>
+      <ProgressBar />
+    </div>
   );
 };
 
