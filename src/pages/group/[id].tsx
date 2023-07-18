cab18d1a0fa188cba4b37a768db7855adc667432
ratbuyer <m1662576219@gmail.com>
2023-07-18 02:08:07 -0400

    group refractor

--- src/pages/group/[id].tsx
+++ src/pages/group/[id].tsx
@@ -1,47 +1,49 @@
-const Countdown = () => (
-  <div className="flex gap-x-2">
-    <div className="flex flex-col items-center">
-      <p className="text-3xl font-bold ">03</p>
-      <p className="text-xs ">Days</p>
-    </div>
-    <p className="text-3xl font-bold "> : </p>
-    <div className="flex flex-col items-center">
-      <p className="text-3xl font-bold ">07</p>
-      <p className="text-xs ">Hour</p>
-    </div>
-    <p className="text-3xl font-bold"> : </p>
-    <div className="flex flex-col items-center">
-      <p className="text-3xl font-bold">20</p>
-      <p className="text-xs">Minute</p>
-    </div>
-  </div>
-);
-
-const GroupCard = () => (
-  <div className="flex w-[20%] flex-col items-center text-gray-800">
-    <img src="/assets/dummy/product.png" className="rounded-full" />
-
-    <p className="mt-5 text-xl">Group name</p>
-
-    <div className="mt-3 flex flex-row gap-x-3">
-      <p className="rounded-full bg-purple-300 px-2 text-purple-800">Candy</p>
-      <p className="rounded-full bg-purple-300 px-2 text-purple-800">Snacks</p>
-    </div>
-
-    <div className="mt-3 flex flex-row gap-x-3">
-      <p>21/50</p>
-      <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
-    </div>
-
-    <p className="mt-5 text-gray-400">max possible discount : 30%</p>
-
-    <button className="mt-3 rounded bg-rose-600 px-20 py-2 text-white">
-      Join group
-    </button>
-  </div>
-);
-
 const Group = () => {
+  const Countdown = () => (
+    <div className="flex gap-x-2">
+      <div className="flex flex-col items-center">
+        <p className="text-3xl font-bold ">03</p>
+        <p className="text-xs ">Days</p>
+      </div>
+      <p className="text-3xl font-bold "> : </p>
+      <div className="flex flex-col items-center">
+        <p className="text-3xl font-bold ">07</p>
+        <p className="text-xs ">Hour</p>
+      </div>
+      <p className="text-3xl font-bold"> : </p>
+      <div className="flex flex-col items-center">
+        <p className="text-3xl font-bold">20</p>
+        <p className="text-xs">Minute</p>
+      </div>
+    </div>
+  );
+
+  const GroupCard = () => (
+    <div className="flex w-[20%] flex-col items-center text-gray-800">
+      <img src="/assets/dummy/product.png" className="rounded-full" />
+
+      <p className="mt-5 text-xl">Group name</p>
+
+      <div className="mt-3 flex flex-row gap-x-3">
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">Candy</p>
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">
+          Snacks
+        </p>
+      </div>
+
+      <div className="mt-3 flex flex-row gap-x-3">
+        <p>21/50</p>
+        <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
+      </div>
+
+      <p className="mt-5 text-gray-400">max possible discount : 30%</p>
+
+      <button className="mt-3 rounded bg-rose-600 px-20 py-2 text-white">
+        Join group
+      </button>
+    </div>
+  );
+
   return (
     <div>
       <div className="relative">
--- src/pages/group/all.tsx
+++ src/pages/group/all.tsx
@@ -15,55 +15,57 @@ const Groups = () => {
     </div>
   );
 
+  const Side = () => (
+    <div className="flex w-[20%] flex-col items-center text-gray-800">
+      <div className="mt-20">
+        <img src="/assets/dummy/product.png" className="rounded-full" />
+      </div>
+
+      <p className="mt-5 text-xl">Group name</p>
+
+      <div className="mt-3 flex flex-row gap-x-3">
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">Candy</p>
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">
+          Snacks
+        </p>
+      </div>
+
+      <p className="mt-5 text-gray-400">max possible discount : 30%</p>
+
+      <div className="flex flex-row gap-x-3">
+        <p>21/50</p>
+        <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
+      </div>
+
+      <div className="mt-10 flex flex-col items-center gap-y-3">
+        <button>
+          <p>Snacks</p>
+        </button>
+        <button>
+          <p>Candies</p>
+        </button>
+        <button>
+          <p>Instant Food</p>
+        </button>
+        <button>
+          <p>Personal Care</p>
+        </button>
+        <button>
+          <p>Beauty</p>
+        </button>
+        <button>
+          <p>Home & Life</p>
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="flex h-full flex-row">
-        <div className="flex w-[20%] flex-col items-center text-gray-800">
-          <div className="mt-20">
-            <img src="/assets/dummy/product.png" className="rounded-full" />
-          </div>
+        <Side />
 
-          <p className="mt-5 text-xl">Group name</p>
-
-          <div className="mt-3 flex flex-row gap-x-3">
-            <p className="rounded-full bg-purple-300 px-2 text-purple-800">
-              Candy
-            </p>
-            <p className="rounded-full bg-purple-300 px-2 text-purple-800">
-              Snacks
-            </p>
-          </div>
-
-          <p className="mt-5 text-gray-400">max possible discount : 30%</p>
-
-          <div className="flex flex-row gap-x-3">
-            <p>21/50</p>
-            <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
-          </div>
-
-          <div className="mt-10 flex flex-col items-center gap-y-3">
-            <button>
-              <p>Snacks</p>
-            </button>
-            <button>
-              <p>Candies</p>
-            </button>
-            <button>
-              <p>Instant Food</p>
-            </button>
-            <button>
-              <p>Personal Care</p>
-            </button>
-            <button>
-              <p>Beauty</p>
-            </button>
-            <button>
-              <p>Home & Life</p>
-            </button>
-          </div>
-        </div>
-
-        <div className=" h-100 border-l border-gray-300"></div>
+        <div className="h-100 border-l border-gray-300"></div>
 
         <div className="w-[80%] p-5">
           <div className="mt-10 flex flex-row flex-wrap gap-x-5">
