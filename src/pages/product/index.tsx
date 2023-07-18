8f96cbd23bc499ffd4a91365c28a7562a80651c9
ratbuyer <m1662576219@gmail.com>
2023-07-18 02:11:55 -0400

    refractor

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -15,50 +15,52 @@ const Products = () => {
     </div>
   );
 
+  const Side = () => (
+    <div className="flex w-[20%] flex-col items-center text-gray-800">
+      <div className="mt-20">
+        <img src="assets/dummy/product.png" className="rounded-full" />
+      </div>
+
+      <p className="mt-5 text-xl">Group name</p>
+
+      <div className="mt-3 flex flex-row gap-x-3">
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">Candy</p>
+        <p className="rounded-full bg-purple-300 px-2 text-purple-800">
+          Snacks
+        </p>
+      </div>
+
+      <p className="mt-5 text-gray-400">max possible discount : 30%</p>
+
+      <div className="flex flex-row gap-x-3">
+        <p>21/50</p>
+        <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
+      </div>
+
+      <div className="mt-10 flex flex-col items-center gap-y-3">
+        <button>
+          <p>All Results(10)</p>
+        </button>
+        <button>
+          <p>Snacks(10)</p>
+        </button>
+        <button>
+          <p>Candies(10)</p>
+        </button>
+        <button>
+          <p>Instant Food(10)</p>
+        </button>
+        <button>
+          <p>Beauty(10)</p>
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className="flex h-full flex-row">
-        <div className="flex w-[20%] flex-col items-center text-gray-800">
-          <div className="mt-20">
-            <img src="assets/dummy/product.png" className="rounded-full" />
-          </div>
-
-          <p className="mt-5 text-xl">Group name</p>
-
-          <div className="mt-3 flex flex-row gap-x-3">
-            <p className="rounded-full bg-purple-300 px-2 text-purple-800">
-              Candy
-            </p>
-            <p className="rounded-full bg-purple-300 px-2 text-purple-800">
-              Snacks
-            </p>
-          </div>
-
-          <p className="mt-5 text-gray-400">max possible discount : 30%</p>
-
-          <div className="flex flex-row gap-x-3">
-            <p>21/50</p>
-            <p className="rounded bg-rose-600 px-2 text-white">5% off</p>
-          </div>
-
-          <div className="mt-10 flex flex-col items-center gap-y-3">
-            <button>
-              <p>All Results(10)</p>
-            </button>
-            <button>
-              <p>Snacks(10)</p>
-            </button>
-            <button>
-              <p>Candies(10)</p>
-            </button>
-            <button>
-              <p>Instant Food(10)</p>
-            </button>
-            <button>
-              <p>Beauty(10)</p>
-            </button>
-          </div>
-        </div>
+        <Side />
 
         <div className="h-100 border-l border-gray-300"></div>
 
