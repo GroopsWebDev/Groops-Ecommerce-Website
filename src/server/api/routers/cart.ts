cee0a31abf9a3aac6d32796a6861fefa0ca460b7
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2023-07-27 16:34:49 +0800

    updated cart api

--- src/server/api/routers/cart.ts
+++ src/server/api/routers/cart.ts
@@ -3,75 +3,94 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const ShoppingCartRouter = createTRPCRouter({
-  getUserCartList: publicProcedure
-    .input(z.object({ userEmail: z.string() }))
+  getUserCartItems: publicProcedure
+    .input(z.object({ user_Clerk_id: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.prisma.cart.findMany({
-        where: { user_email: input.userEmail },
+        where: { user_Clerk_id: input.user_Clerk_id },
       });
     }),
-
-  // addCartItem: publicProcedure
-  // .input(z.object({ userEmail: z.string(), skuid: z.string() }))
-  // .mutation(({ ctx, input }) => {
-  //   return ctx.prisma.cart.upsert({
-  //     where: { user_email: input.userEmail },
-  //     create: {
-  //       user_email: input.userEmail,
-  //       quantity: 1,
-  //       user: { connect: { email: input.userEmail } },
-  //       product: { create: [{ skuid: input.skuid }] },
-  //     },
-  //     update: { quantity: { increment: 1 } },
-  //   });
-  // }),
 
   addCartItem: publicProcedure
-    .input(z.object({ userEmail: z.string(), skuid: z.string() }))
+    .input(
+      z.object({
+        user_Clerk_id: z.string(),
+        product_id: z.string(),
+        quantity: z.number().min(1),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-      const cart = await ctx.prisma.cart.upsert({
-        where: { user_email: input.userEmail },
-        create: {
-          user_email: input.userEmail,
-          quantity: 1,
-          user: { connect: { email: input.userEmail } },
+      //check if the product exists
+      const { user_Clerk_id, product_id } = input;
+      // Check if a matching cart item already exists
+      const existingCartItem = await ctx.prisma.cart.findUnique({
+        where: {
+          user_Clerk_id_product_id: {
+            user_Clerk_id,
+            product_id,
+          },
         },
-        update: { quantity: { increment: 1 } },
-        include: { product: true },
+      });
+      if (existingCartItem) {
+        // If the matching cart item exists, increment the quantity by 1
+        const updatedCartItem = await ctx.prisma.cart.update({
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 },
+        });
+        return updatedCartItem;
+      } else {
+        // If the matching cart item doesn't exist, create a new cart item with quantity 1
+        const newCartItem = await ctx.prisma.cart.create({
+          data: {
+            quantity: 1,
+            user_Clerk_id,
+            product: { connect: { skuid: product_id } },
+          },
+        });
+        return newCartItem;
+      }
+    }),
+
+  reduceCartItem: publicProcedure
+    .input(
+      z.object({
+        user_Clerk_id: z.string(),
+        product_id: z.string(),
+        remove_item: z.boolean().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      //check if the product exists
+      const { user_Clerk_id, product_id, remove_item } = input;
+      // Check if a matching cart item already exists
+      const existingCartItem = await ctx.prisma.cart.findUnique({
+        where: {
+          user_Clerk_id_product_id: {
+            user_Clerk_id,
+            product_id,
+          },
+        },
       });
 
-      if (!cart.product) {
-        // If the product relation is empty, create a new product
-        const product = await ctx.prisma.product.create({
-          data: { skuid: input.skuid, cart: { connect: { id: cart.id } } },
-        });
-
-        cart.product = [product];
-      } else {
-        // If the product relation already exists, add a new product to it
-        const product = await ctx.prisma.product.create({
-          data: { skuid: input.skuid, cart: { connect: { id: cart.id } } },
-        });
-
-        cart.product.push(product);
+      if (!existingCartItem) {
+        throw new Error("Cart item not found");
       }
 
-      return cart;
-    }),
-
-  deleteItem: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.cart.delete({
-        where: { ...input },
-      });
-    }),
-
-  deleteAllInCart: publicProcedure
-    .input(z.object({ userId: z.string() }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.cart.deleteMany({
-        where: { userId: input.userId },
-      });
+      if (remove_item) {
+        await ctx.prisma.cart.delete({ where: { id: existingCartItem.id } });
+        return null;
+      } else {
+        if (existingCartItem.quantity > 1) {
+          const updatedCartItem = await ctx.prisma.cart.update({
+            where: { id: existingCartItem.id },
+            data: { quantity: existingCartItem.quantity - 1 },
+          });
+          return updatedCartItem.quantity > 0 ? updatedCartItem : null;
+        } else {
+          // If reduceQuantity is true and quantity is 1, remove the cart item
+          await ctx.prisma.cart.delete({ where: { id: existingCartItem.id } });
+          return null;
+        }
+      }
     }),
 });
