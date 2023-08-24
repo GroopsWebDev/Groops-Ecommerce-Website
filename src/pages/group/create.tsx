f20167121e9ba53598a894c03686b2b4f580acab
ratbuyer <m1662576219@gmail.com>
2023-08-24 00:03:39 -0400

    group create

--- src/pages/group/create.tsx
+++ src/pages/group/create.tsx
@@ -5,6 +5,87 @@ import ProgressBar from "@components/group/progressBar";
 const CreateGroupPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
+  const Discount = () => {
+    const Selector = () => (
+      <select
+        id="size"
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+      >
+        <option value="20">2% off (20 members)</option>
+        <option value="20">8% off (50 members)</option>
+      </select>
+    );
+
+    return (
+      <div className="flex w-full justify-between gap-x-5 p-5 text-gray-800">
+        <div className="w-[30%] p-5">
+          <h1 className="text-2xl">Group Discount</h1>
+          <p className="text-gray-400">
+            Decide group discount by selecting group size and category
+          </p>
+        </div>
+
+        <div className="w-[60%] rounded-xl bg-white p-5 text-gray-700">
+          <p className="">Base Discoutn - Group Size</p>
+          <p className=" mb-1 mt-1 text-gray-400">
+            select the maximum number of group members
+          </p>
+          <Selector />
+
+          <p className="mt-10">Category Discount - Group Category Labels</p>
+          <p className="mt-1 text-gray-400">
+            Items selected have extra discount (Maximum 2)
+          </p>
+
+          <div className="mt-5 grid grid-cols-3 gap-4">
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+            <button className="rounded-full border bg-gray-100 p-3">
+              5% - Snack
+            </button>
+          </div>
+
+          <p className="mt-10">Estimated Total Discount</p>
+          <p className="mt-1 text-gray-400">
+            estimated discount is based on the group size and category labels
+          </p>
+
+          <div className="mt-5 flex justify-between">
+            <p>
+              Estimated <span className="font-bold">Based</span> Discount
+            </p>
+            <p className="font-bold text-rose-600">2%</p>
+          </div>
+          <div className="mt-5 flex justify-between">
+            <p>
+              Discount on <span className="font-bold">Candies</span>
+            </p>
+            <p className="font-bold text-rose-600">2%</p>
+          </div>
+          <div className="mt-5 flex justify-between">
+            <p>
+              Discount on <span className="font-bold">Pearsonal Care</span>
+            </p>
+            <p className="font-bold text-rose-600">2%</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const Profile = () => (
     <div className="flex w-full justify-between gap-x-5 p-5">
       <div className="w-[30%] p-5">
@@ -78,19 +159,23 @@ const CreateGroupPage = () => {
     </div>
   );
 
+  const pageTable = [<Profile />, <Discount />];
+
   return (
     <>
       <div>
         <ProgressBar current={currentPage} />
       </div>
       <div className="w-full bg-gray-200 py-10">
-        <Profile />
+        {pageTable[currentPage - 1]}
         <div className="mt-10 flex justify-between px-5">
           <button className="rounded border border-rose-600 bg-white px-7 py-2 text-rose-600">
             Back
           </button>
           <button
-            onClick={() => setCurrentPage(currentPage + 1)}
+            onClick={() =>
+              currentPage < 6 ? setCurrentPage(currentPage + 1) : null
+            }
             className="rounded border border-rose-600 bg-rose-600 px-7 py-2 text-white"
           >
             Next
